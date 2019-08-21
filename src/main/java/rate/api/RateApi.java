9064b915da6e77f52af65f4c66b1f8b0851a81da
André Rafael Mezzalira <andrerafaelmezzalira@gmail.com>
2019-08-21 09:25:18 -0300

    cross origin

--- src/main/java/rate/api/RateApi.java
+++ src/main/java/rate/api/RateApi.java
@@ -3,6 +3,7 @@ package rate.api;
 import java.util.SortedSet;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import rate.service.RateService;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000"})
 public class RateApi {
 
 	@Autowired
